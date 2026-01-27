0dd3b10dec1d5827efa5e1a14978626ca561b96a
ygor-h <ygorlh01@gmail.com>
2026-01-26 23:20:51 -0300

    feat: adiciona CRUD completo de simulações

--- src/main/java/com/ygorhenrique/financial_simulation_api/controller/SimulationController.java
+++ src/main/java/com/ygorhenrique/financial_simulation_api/controller/SimulationController.java
@@ -1,15 +1,15 @@
 package com.ygorhenrique.financial_simulation_api.controller;
 
+import com.ygorhenrique.financial_simulation_api.domain.simulation.Simulation;
 import com.ygorhenrique.financial_simulation_api.dto.SimulationDTO;
 import com.ygorhenrique.financial_simulation_api.service.SimulationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/simulation")
@@ -21,5 +21,21 @@ public class SimulationController {
     public ResponseEntity<Void> saveSimulation(@Valid @RequestBody SimulationDTO simulationDTO) {
         simulationService.saveSimulation(simulationDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Simulation>> getAllSimulations() {
+        return ResponseEntity.ok(simulationService.getAllSimulations());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Simulation> getSimulationById(@PathVariable Long id) {
+        return ResponseEntity.ok(simulationService.getSimulationById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSimulationById(@PathVariable Long id) {
+        simulationService.deleteSimulation(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/ygorhenrique/financial_simulation_api/service/SimulationService.java
+++ src/main/java/com/ygorhenrique/financial_simulation_api/service/SimulationService.java
@@ -6,6 +6,7 @@ import com.ygorhenrique.financial_simulation_api.repository.SimulationRepository
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class SimulationService {
@@ -15,12 +16,29 @@ public class SimulationService {
         this.simulationRepository = simulationRepository;
     }
 
-    public void saveSimulation (SimulationDTO simulationDTO) {
+    public void saveSimulation(SimulationDTO simulationDTO) {
         Simulation simulation = new Simulation();
         simulation.setInitialValue(simulationDTO.getInitialValue());
         simulation.setInterestRate(simulationDTO.getInterestRate());
         simulation.setPeriodInMonths(simulationDTO.getPeriodInMonths());
         simulation.setCreatedAt(LocalDateTime.now());
         simulationRepository.save(simulation);
+    }
+
+    public List<Simulation> getAllSimulations() {
+        return simulationRepository.findAll();
+    }
+
+    public Simulation getSimulationById(Long id) {
+        return simulationRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("ID não encontrado")
+        );
+    }
+
+    public void deleteSimulation(Long id) {
+        Simulation simulation = simulationRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("ID não encontrado")
+        );
+        simulationRepository.delete(simulation);
     }
 }
